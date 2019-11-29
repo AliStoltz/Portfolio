77cc6e52d9f719aca8408e29944fa26d1f754075
AliStoltz <ali.mstoltz@gmail.com>
2019-11-29 10:28:44 -0800

    adding jquery and ajax for dynamic text

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('danny is an idiot');
+// console.log('danny is an idiot');
+// var about = ['Software Engineer', 'Web Developer', 'Researcher', 'Guitar Enthusiast'];
+// var elem = document.getElementById('changeText');
+// var inst = setInterval(change, 1000);
+
+// function change() {
+//   elem.innerHTML = text[counter];
+//   counter++;
+//   if (counter >= text.length) {
+//     counter = 0;
+//   };
+// };
+
+// change();
+
+$(function () {
+  count = 0;
+  wordsArray = ['Software Engineer', 'Web Developer', 'Researcher', 'Guitar Enthusiast'];
+  setInterval(function () {
+    count++;
+    $('#changeText').fadeOut(400, function () {
+      $(this).text(wordsArray[count % wordsArray.length]).fadeIn(400);
+    });
+  }, 2000);
+});
